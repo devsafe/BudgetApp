353b7f6f3afe37f7d6067c2b5d46594ab29912da
Rafinad <rafinad@outlook.com>
2021-05-07 21:42:46 +0300

    init

--- BudgetApp/ContentView.swift
+++ BudgetApp/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationBarTitle("My expenses")
+            .navigationBarItems(trailing: Button(action: {
+                let expense = ExpenseItem(name: "Test", type: "Personal", amount: 50)
+                self.expenses.items.append(expense)
+            }) {
+                Image(systemName: "plus")
+            })
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
